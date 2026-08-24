a0e9bb23ff29b53db9e45109a1434052758e8ce3
Lucas Gabriel <lucasferreiraperes29@gmail.com>
2026-08-24 18:10:19 -0300

    Início da lista de frutas, até o momento fiz uma caixa de pesquisa e estou tentando fazer uma forma de adicionar mais elementos

---
+++ script.js
@@ -0,0 +1,14 @@
+const title = document.querySelector(#h1)
+
+const input = document.getElementById('busca')
+const lista = document.getElementById('resultados')
+
+input.addEventListener('input', (e) => {
+    const termo = e.target.value.toLowerCase()
+}
+);
+
+const lista = document.createElement('busca');
+novoItem.text.textContent = 'Novo item'
+
+lista.appendChild(novoItem)
