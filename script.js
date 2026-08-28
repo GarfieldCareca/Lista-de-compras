0f3286e01c60a0bb070d260e7f6baaa244a89bad
Lucas Gabriel <lucasferreiraperes29@gmail.com>
2026-08-28 10:37:06 -0300

    agora está funcionando a adição de produtos

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const entrada = document.getElementById("entrada");
 const botao = document.getElementById("adicionar");
 const lista = document.getElementById("lista");
+const conteiner = document.getElementById("conteiner")
 
 botao.addEventListener("click", () => {
     const texto = entrada.value;
 
-    const item = document.createElement("ul");
+    const item = document.createElement("li");
 
     item.textContent = texto;
 
     lista.appendChild(item);
+
+    conteiner.appendChild(lista)
 
     entrada.value = "";
 });
